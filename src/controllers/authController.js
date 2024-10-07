df9e58959b26ee4601ef52d79defd7ac9da9acb8
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2024-10-07 11:40:44 -0500

    Ajuste autenticacion a un usuario quemado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,12 @@ const { SECRET_KEY } = require('../config/jwtConfig');
 
 const login = (req, res) => {
   const userId = '1'; // ID del usuario
-  const email = 'user@example.com'; // Ejemplo de email
+  const email = 'admin@mail.com'; // Ejemplo de email
+  const password = 'admin';
+  console.log(req.body)
+  if(req.body.email !== email || req.body.password !== password){
+    return res.status(400).json({ error: 'Credenciales inválidas' });
+  }
 
   // Crear el token con userId y email
   const token = jwt.sign({ userId, email }, SECRET_KEY, { expiresIn: '1h' });
@@ -14,5 +19,6 @@ const login = (req, res) => {
 const protectedRoute = (req, res) => {
   res.send(`Ruta protegida. Tu id de usuario es ${req.userId}`);
 };
+
 
 module.exports = { login, protectedRoute };
